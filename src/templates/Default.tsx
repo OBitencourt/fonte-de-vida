14fd6c1b98614e481bb47704a354439323a41ebf
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-11-06 19:51:20 -0100

    feat: adding initial structure

---
+++ src/templates/Default.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Header from '../components/Header'
+// import Footer from '../components/Footer'
+
+interface DefaultProps {
+    children: React.ReactNode;
+}
+
+const Default = ({children}: DefaultProps) => {
+
+    return (
+        <>
+            Header
+            {children}
+            Footer
+        </>
+    )
+}
+
+export default Default
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#210f33',
+        },
+        secondary: {
+            main: '#ffffff',
+        },
+        background: {
+            default: '#E3F2FD',
+            white: '#ffffff'
+        }
+    },
+})
+
+export default theme
